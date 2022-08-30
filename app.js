6fa39ade73b724324825488e4ebdb79efdef1634
eu <diego_thegarden@hotmail.com>
2022-08-30 21:03:37 +0200

    headers

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit(); //salir do event loop
+  console.log(req.headers);
+  //   process.exit(); //salir do event loop
+  res.setHeader("Content-Type", "text/html");
+  res.write("<head><title>Enter Message</title><head>");
+  res.write(
+    '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+  );
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
