010dcd3bac827c5ca5fd54b85f15528e5aa259c6
eu <diego_thegarden@hotmail.com>
2022-08-31 22:50:42 +0200

    data streams

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     //   process.exit(); //salir do event loop
     res.write("<head><title>Enter Message</title><head>");
@@ -11,6 +13,26 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+  if (url === "/message" && method === "POST") {
+    //recoller o steam de datos
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<head><title>Hi</title><head>");
   res.write("<body><h1>Hello</h1></body>");
