8ab037d27a690be96b03bee75673762796f4d76a
eu <diego_thegarden@hotmail.com>
2022-09-04 16:17:27 +0200

    public dir

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const main = require('require-main-filename')()
-
-const adminRoutes = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
+const main = require("require-main-filename")();
 
 //next permite á request continuar ao seguite middle (se non se usa debe acabar con unha response)
 const app = express();
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 //crea un middleware para convertir o body da fiorm da request a json
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Uso das rutas de router en vez de app.
 //"/admin" filtra so as q comecen por eso
